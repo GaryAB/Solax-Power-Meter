5eebe91787c41fd3d7ff354fce5cf03ec719924e
Gary Bainbridge <130378533+GaryAB@users.noreply.github.com>
2026-09-19 19:32:40 +0100

    Error correction
    Routiine failed when receiving blank data from NESO API. Now trapped.

---
+++ Scripts/Octopus5.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import datetime
+import Settings
+
+def check():
+
+  global data
+  global date
+  region = Settings.region
+  data = None
+  date = None
+  endTime = None
+  startTime = ""
+  
+  id = 0
+
+  try:
+    localTime = datetime.datetime.now()
+    date = (str(localTime)[0:10])
+    url = 'https://api.neso.energy/api/3/action/datastore_search?resource_id=3ebf77d7-05df-466e-a023-dc45a90efeea'
+    sessDate = date
+    stop = 0
+    stop1 = 0
+
+    while (sessDate == date) and stop == 0:
+      x = requests.post(url)
+      x = (x.text)
+      x = json.loads(x)
+      y = x["success"]
+      if y != True:
+        return ("",0,0,0,0,0)
+      y = x["result"]
+      z = y["records"]
+      del x
+      id = 0
+         
+      while (id < 100) and (stop == 0):
+        record = z[id]
+        supplier = record["Registered DFS Participant"]
+        sessDate = record["Delivery Date"]
+        direction = record["Event Type"]
+        if (supplier != "OCTOPUS ENERGY LIMITED"):
+          id = id + 1
+          continue
+        if sessDate != date:
+          id = id + 1
+          continue
+        status = record["Status"]        
+        if status != "Accepted":
+          id = id + 1
+          continue
+        level = (record["Zone"])
+        if level != region:
+          id = id + 1
+          continue
+        
+        if record["From_Local"] != endTime:
+          startTime = record["From_Local"]
+        endTime = record["To_Local"]
+        sessDate = record["Delivery Date"]
+        status = record["Status"]
+        startHour = int(startTime[0:2])
+        startMins = int(startTime[3:5])
+        startComp = (startHour * 60) + startMins - 10
+        startData = startHour + (startMins * 256)
+        endHour = int(endTime[0:2])
+        endMins = int(endTime[3:5])
+        stop = 0
+        id = id +1
+
+      id = 0  
+      p = y["_links"]
+      q = p["next"]
+      url = "https://api.neso.energy" + q
+
+    if (startTime != "") and (direction == "Downwards"):
+      return ("Saving session today " + startTime + " to " + endTime,startData,startHour,startMins,endHour,endMins,direction)  
+    elif (startTime != "") and (direction == "Upwards"):
+      return ("Free session today " + startTime + " to " + endTime,startData,startHour,startMins,endHour,endMins,direction)  
+    else:
+      return ("",0,0,0,0,0,"")
+
+  except: 
+    return ("",0,0,0,0,0,"")
